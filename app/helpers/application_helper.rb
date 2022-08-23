11feae6d5ff723744e70405f5b8642b2486da54c
Dmitry Shyrokonis <azerorh@gmail.com>
2022-08-23 06:39:43 +0200

    articles link on pdf when from mobile, gitignore uploads dir added

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,12 @@ module ApplicationHelper
     end
   end
 
+  def mobile_device?
+    if session[:mobile_param]
+      session[:mobile_param] == "1"
+    else
+      request.user_agent =~ /Mobile|webOS/
+    end
+  end
+
 end
